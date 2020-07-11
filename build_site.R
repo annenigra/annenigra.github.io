ebfa329161f48573e8085ed3992e28a1f1943ef3
Annie Nigra <aen2136@cumc.columbia.edu>
2020-07-11 10:47:30 -0400

    Add files via upload

--- build_site.R
+++ build_site.R
@@ -8,3 +8,8 @@ setwd("~/Desktop/annenigra.github.io")
 rmarkdown::render_site()
 
 
+# Keeping track of colors on in the sytel2.css sheet
+# Hyperlink colors: #5b9ea6
+# hyperlinke hover colors: #73947d --> #984328
+# Title bar color: 3e3f3a 
+
