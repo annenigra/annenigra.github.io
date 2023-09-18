3942e8476049932cbb1f4b8af1aa3c0d5998b29e
Annie Nigra <aen2136@cumc.columbia.edu>
2023-09-18 06:25:31 -0400

    Add files via upload

--- build_site.R
+++ build_site.R
@@ -1,14 +1,12 @@
 #Set our working directory. 
-#This helps avoid confusion if our working directory is 
-#not our site because of other projects we were 
-#working on at the time. 
+
 setwd("~/Desktop/annenigra.github.io")
 
 #render your sweet site. 
 rmarkdown::render_site()
 
 
-# Keeping track of colors on in the sytel2.css sheet
+# Keeping track of colors on in the style2.css sheet
 # Hyperlink colors: #5b9ea6
 # hyperlinke hover colors: #73947d --> #984328
 # Title bar color: 3e3f3a 
