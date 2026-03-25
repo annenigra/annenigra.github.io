759eb4306685feb32d6f3eca70781a85b16f8c46
Annie Nigra <annenigra@gmail.com>
2026-03-24 21:09:28 -0400

    describe what you changed

---
+++ update_site.R
@@ -0,0 +1,7 @@
+rmarkdown::render_site()
+
+# Push to GitHub in the Terminal:
+#cd "/Users/aen2136/Desktop/annenigra.github.io"
+#git add .
+#git commit -m "describe what you changed"
+#git push origin main
